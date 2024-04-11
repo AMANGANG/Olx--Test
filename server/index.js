72d2070b181f5d094761bd1d7f1bae293c29c4cf
Aman Gangwar <lcs2020049@iiitl.ac.in>
2024-04-11 10:46:47 +0530

    Add server code to handle POST request on root path

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import connectDataBase from './config/Database.js';
+import router from './routes/InventoryPath.js';
 
 const app = express();
 const PORT = 3000;
-
+app.use(express.json());
+connectDataBase();
 app.get('/', (req, res) => {
     res.send('Hello World');
     }); 
-
+app.use("/",router)
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
     });   
