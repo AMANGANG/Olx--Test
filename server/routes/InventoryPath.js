1ecd55a574674778b6e928b43f672e835dcd589f
Aman Gangwar <lcs2020049@iiitl.ac.in>
2024-04-11 13:11:56 +0530

    Add GET routes for fetching all inventory and single inventory by ID

--- server/routes/InventoryPath.js
+++ server/routes/InventoryPath.js
@@ -3,4 +3,6 @@ import Createinventory from '../controllers/CreateInventory.js';
 
 const router = express.Router();
 router.post('/inventory', Createinventory.createinventory);
+router.get('/inventory', Createinventory.getinventory);
+router.get('/inventory/:id', Createinventory.getinventorybyid);
 export default router;
