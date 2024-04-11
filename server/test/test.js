bc96d950eb4eaaefb84b97b166959c94206dea64
Aman Gangwar <lcs2020049@iiitl.ac.in>
2024-04-11 14:23:21 +0530

    Add a test to check createinventory function save method

--- server/test/test.js
+++ server/test/test.js
@@ -1,8 +1,48 @@
-import assert from 'assert';
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+import sinon from 'sinon';
+import Inventory from '../models/InventoryDataSchema.js';
+import Createinventory from '../controllers/CreateInventory.js';
+
+
+describe('Createinventory', function() {
+  describe('createinventory', function() {
+    it('should save new inventory', async function() {
+      const mockInventory = sinon.mock(Inventory.prototype);
+      mockInventory.expects('save').once();
+      //Arrange
+      const req = {
+        body: {
+          status: 'Created',
+          sku: '12345',
+          type: 'Car',
+          primarystatus: 'Available',
+          primarylocation: 'Warehouse A',
+          attributes: {
+            VIN: '1HGCM82633A123456',
+            make: 'Honda',
+            model: 'Accord',
+            trim: 'EX',
+            year: 2023
+          },
+          pricing: {
+            cost: 20000,
+            sellingPrice: 25000
+          },
+          metadata: {}
+        }
+      };
+
+      const res = {
+        status: function() {
+          return this;
+        },
+        json: function() {}
+      };
+      //Act
+      await Createinventory.createinventory(req, res);
+      //Assert
+      mockInventory.verify();
+      mockInventory.restore();
+      
     });
   });
 });
