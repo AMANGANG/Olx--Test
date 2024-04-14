515fb8c2e483b4755eb6fee6c55652023b2f4b2f
Aman Gangwar <lcs2020049@iiitl.ac.in>
2024-04-14 12:06:14 +0530

    Refactor InventoryPath.js to use ManageInventory controller for inventory management and add the deletepath

--- server/routes/InventoryPath.js
+++ server/routes/InventoryPath.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import Createinventory from '../controllers/CreateInventory.js';
+import ManageInventory from '../controllers/ManageInventory.js';
 
 const router = express.Router();
-router.post('/inventory', Createinventory.createinventory);
-router.get('/inventory', Createinventory.getinventory);
-router.get('/inventory/:id', Createinventory.getinventorybyid);
-router.put('/inventory/:id', Createinventory.updateinventory);
+router.post('/inventory', ManageInventory.createinventory);
+router.get('/inventory', ManageInventory.getinventory);
+router.get('/inventory/:id', ManageInventory.getinventorybyid);
+router.put('/inventory/:id', ManageInventory.updateinventory);
+router.delete('/inventory/:id', ManageInventory.deleteinventory);
 export default router;
