00ffc14712df8b8a60c20703f1b60f787f8254d4
Aman Gangwar <lcs2020049@iiitl.ac.in>
2024-04-21 14:38:29 +0530

    Refactor ManageInventory controller to use ResponseInventory controller for managing response data

--- server/controllers/ManageInventory.js
+++ server/controllers/ManageInventory.js
@@ -1,5 +1,6 @@
 import InventoryData from "./InventoryData.js";
 import Inventory from "../models/InventoryDataSchema.js";
+import resposnse from "../controllers/ResponseInventory.js";
 
 class ManageInventory {
     async createinventory(req, res) {
@@ -7,15 +8,18 @@ class ManageInventory {
         const newinventory = new Inventory(inventorydata);
         try {
             await newinventory.save();
-            res.status(201).json(newinventory);
+
+            const resposnseIdata = new resposnse(newinventory.status, newinventory.SKU, newinventory.type, newinventory.pricing, newinventory.metadata);
+            res.status(201).json(resposnseIdata);
         } catch (error) {
             res.status(409).json({message:error.message});
         }
     }
     async getinventories (req, res) {
         try {
-            const inventory = await Inventory.find().select('-_id');
-            res.status(200).json(inventory);
+            const inventory = await Inventory.find();
+            const resposnsedata= inventory.map((inventory) => new resposnse(inventory.status, inventory.SKU, inventory.type, inventory.pricing, inventory.metadata));
+            res.status(200).json(resposnsedata);
         } catch (error) {
             res.status(404).json({message:error.message});
         }
@@ -28,7 +32,8 @@ class ManageInventory {
             if (!inventory) {
                 return res.status(404).json({ message: 'Inventory not found' });
             }
-            res.status(200).json(inventory);
+            const resposnsedata= new resposnse(inventory.status, inventory.SKU, inventory.type, inventory.pricing, inventory.metadata);
+            res.status(200).json(resposnsedata);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
