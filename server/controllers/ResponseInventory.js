47096ceffd1f562c242df1a6d8fa77ff03047590
Aman Gangwar <lcs2020049@iiitl.ac.in>
2024-04-21 14:38:12 +0530

    Add ResponseInventory controller for managing response data

---
+++ server/controllers/ResponseInventory.js
@@ -0,0 +1,10 @@
+class resposnse {
+    constructor(status, sku, type, pricing, metadata) {
+        this.status = status;
+        this.sku = sku;
+        this.type = type;
+        this.pricing = pricing;
+        this.metadata = metadata;
+    }
+}   
+export default resposnse;
